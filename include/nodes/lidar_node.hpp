a5f4851833171f822fd83636e982dcf813adc3e6
Wojnyss <wojnar.krystof@gmail.com>
2025-04-17 12:07:31 +0200

    lidar last update

---
+++ include/nodes/lidar_node.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <mutex>
+
+namespace nodes {
+
+    class LidarNode : public rclcpp::Node {
+    public:
+        LidarNode();
+
+        float get_forward_distance() const;
+        float get_back_distance() const;
+        float get_left_distance() const;
+        float get_right_distance() const;
+
+    private:
+        void on_lidar_msg(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+        float get_range_at_angle(const sensor_msgs::msg::LaserScan::SharedPtr& msg, float angle_rad);
+
+        rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_subscriber_;
+
+        mutable std::mutex data_mutex_;
+        float forward_{NAN}, back_{NAN}, left_{NAN}, right_{NAN};
+    };
+
+} // namespace nodes
