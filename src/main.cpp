2dc3a6ed9a4da6098e3b6d636f9d72fd304877ab
Wojnyss <wojnar.krystof@gmail.com>
2025-05-05 12:42:09 +0200

    zkouska 1.1

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@ constexpr float FORWARD_SPEED = 0.3f;
 #ifdef RING
 constexpr float FRONT_STOP_DIST = 0.25f;
 constexpr float FORWARD_SPEED = 0.20f;
+constexpr float MIN_VALID_CENTER_DIST = 0.05f;
 #endif
 
 constexpr float MAX_TURN_ANGLE = 10.0f;
@@ -38,6 +39,7 @@ float round_to_nearest_rad90(float angle_rad) {
 enum class RingState {
     FOLLOW_WALL,
     INITIATE_TURN,
+    INITIATE_INTERSECTION,
     TURNING
 };
 
@@ -47,6 +49,7 @@ int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
     // int aruco = 0; // 0=rovne, 1=vlevo, 2=vpravo
     int aruco = -1;
+    int turn = 0;
 
     ;
 
@@ -107,6 +110,7 @@ int main(int argc, char* argv[]) {
 
     while (rclcpp::ok()) {
         float front = lidar_node->get_forward_distance();
+        float back = lidar_node->get_back_distance();
         if (io_node->get_button_pressed() == 0) stop_requested = true;
 
         std::vector<float> left_beams = lidar_node->get_average_distances_around_angle(-M_PI_2, 5);
@@ -119,6 +123,7 @@ int main(int argc, char* argv[]) {
 
         switch (ring_state) {
             case RingState::FOLLOW_WALL: {
+                std::cout <<  "STATE: FOLLOW_WALL" << std::endl;
                 float virtual_left = left;
                 float virtual_right = right;
 
@@ -137,13 +142,14 @@ int main(int argc, char* argv[]) {
                 bool left_free = std::isfinite(left) && left > CELL_WIDTH;
                 bool right_free = std::isfinite(right) && right > CELL_WIDTH;
                 bool front_free = std::isfinite(front) && front > FRONT_STOP_DIST;
+                bool back_free = std::isfinite(back) && back > FRONT_STOP_DIST;
 
                 const auto& detected = camera_node->get_last_detected();
                 if (!detected.empty())
                 {
                     int id = detected[0].id;
-                    // RCLCPP_INFO(camera_node->get_logger(), "[ARUCO] Detekováno ID: %d", id);
-#ifdef EXIT
+                    RCLCPP_INFO(camera_node->get_logger(), "[ARUCO] Detekováno ID: %d", id);
+#ifdef EXIT.
                     if (id == 0) aruco = 0;
                     else if (id == 1) aruco = 1;
                     else if (id == 2) aruco = 2;
@@ -156,35 +162,88 @@ int main(int argc, char* argv[]) {
                     // RCLCPP_INFO(camera_node->get_logger(), "[ARUCO] Uloženo jako aruco = %d", aruco);
                 }
 
-                if (!front_free || left_free || right_free) {
+                //DETEKCE KRIZOVATEK
+                    float pos_in_cell = fmod(front, CELL_WIDTH); // pozice v rámci jedné buňky
+                    bool centered = fabs(pos_in_cell - CELL_WIDTH/2) <= MIN_VALID_CENTER_DIST;
+                    int num_of_exits = (front_free + left_free + right_free + back_free);
+
+                if (centered)
+                    std::cout << "ARUCO_VAL: "<< aruco << std::endl;
+
+                if (centered && num_of_exits == 2 && !front_free)//zatacka L R
+                {
                     odom.drive(0.0f, 0.0f);
+                    if (right_free)
+                        turn = 1;
+                    else if (left_free)
+                        turn = -1;
                     ring_state = RingState::INITIATE_TURN;
+                }
+                else if (centered && num_of_exits == 1 && !front_free)
+                {
+                    odom.drive(0.0f, 0.0f);
+                    turn = 2;
+                    ring_state = RingState::INITIATE_TURN;
+                }
+                else if (centered && num_of_exits == 3 && aruco != 0) //T
+                {
+                    odom.drive(0.0f, 0.0f);
+                    ring_state = RingState::INITIATE_INTERSECTION;
+                }
+                else if (centered && num_of_exits == 4 && aruco != 0) //+
+                {
+                    odom.drive(0.0f, 0.0f);
+                    ring_state = RingState::INITIATE_INTERSECTION;
                 }
 
                 io_node->turn_on_leds({0, 0, 0, 0, 100, 0, 0, 100, 0, 0, 100, 0});
                 break;
             }
             case RingState::INITIATE_TURN: {
+                std::cout <<  "STATE: INITIATE_TURN" << std::endl;
                 turn_start_yaw = imu_node->getIntegratedResults();
                 float direction = 0.0f;
-                if (aruco == 1) {
+                if (turn == -1) {       //VLEVO
                     direction = 1.0f;
                     ring_state = RingState::TURNING;
-                    io_node->turn_on_leds({100, 100, 0, 100, 0, 0, 0, 100, 0, 0, 100, 0});
-                } else if (aruco == 2) {
+                    io_node->turn_on_leds({0, 100, 0, 0, 0, 0, 0, 100, 0, 0, 100, 0});
+                } else if (turn == 1) { //VPRAVO
                     direction = -1.0f;
                     ring_state = RingState::TURNING;
-                    io_node->turn_on_leds({0, 100, 0, 0, 0, 100, 100, 100, 0, 0, 100, 0});
-                } else if (aruco == 0) {
-                    direction = 0.0f;
-                    ring_state = RingState::FOLLOW_WALL;
+                    io_node->turn_on_leds({0, 100, 0, 0, 100, 0, 0, 0, 0, 0, 100, 0});
+                } else if (turn == 2) { //DOZADU
+                    direction = -2.0f;
+                    ring_state = RingState::TURNING;
+                    io_node->turn_on_leds({0, 100, 0, 0, 100, 0, 0, 100, 0, 0, 0, 0});
                 } else {
                     ring_state = RingState::FOLLOW_WALL;
                 }
                 target_yaw = round_to_nearest_rad90(nodes::ImuNode::normalize_angle(turn_start_yaw + direction * M_PI_2));
                 break;
             }
+            case RingState::INITIATE_INTERSECTION: {
+                        std::cout <<  "STATE: INITIATE_INTERSECTION" << std::endl;
+                        turn_start_yaw = imu_node->getIntegratedResults();
+                        float direction = 0.0f;
+                        if (aruco == 1) {       //VLEVO
+                            direction = 1.0f;
+                            ring_state = RingState::TURNING;
+                            io_node->turn_on_leds({0, 100, 0, 0, 0, 0, 0, 100, 0, 0, 100, 0});
+                        } else if (aruco == 2) { //VPRAVO
+                            direction = -1.0f;
+                            ring_state = RingState::TURNING;
+                            io_node->turn_on_leds({0, 100, 0, 0, 100, 0, 0, 0, 0, 0, 100, 0});
+                        // } else if (aruco == 0) { //ROVNE
+                        //     direction = 0.0f;
+                        //     ring_state = RingState::FOLLOW_WALL;
+                        } else {
+                            ring_state = RingState::FOLLOW_WALL;
+                        }
+                        target_yaw = round_to_nearest_rad90(nodes::ImuNode::normalize_angle(turn_start_yaw + direction * M_PI_2));
+                        break;
+            }
             case RingState::TURNING: {
+                std::cout <<  "STATE: TURNING" << std::endl;
                 float current_yaw = imu_node->getIntegratedResults();
                 float yaw_error = nodes::ImuNode::normalize_angle(target_yaw - current_yaw);
                 float correction = pid_turn.step(yaw_error, dt);
@@ -196,11 +255,11 @@ int main(int argc, char* argv[]) {
 
                 if (std::fabs(yaw_error) < ANGLE_TOLERANCE) {
                     odom.drive(0.0f, 0.0f);
-                    io_node->turn_on_leds({100, 100, 0, 0, 0, 100, 100, 100, 0, 0, 0, 100});
+                    // io_node->turn_on_leds({100, 100, 0, 0, 0, 100, 100, 100, 0, 0, 0, 100});
                     RCLCPP_INFO(io_node->get_logger(), "[TURN] Otocka dokoncena. Spoustim rekalibraci...");
-                    imu_node->setMode(nodes::ImuNodeMode::CALIBRATE);
-                    rclcpp::sleep_for(std::chrono::seconds(1));
-                    imu_node->setMode(nodes::ImuNodeMode::INTEGRATE);
+                    // imu_node->setMode(nodes::ImuNodeMode::CALIBRATE);
+                    // rclcpp::sleep_for(std::chrono::seconds(1));
+                    // imu_node->setMode(nodes::ImuNodeMode::INTEGRATE);
                     ring_state = RingState::FOLLOW_WALL;
                     rclcpp::sleep_for(std::chrono::seconds(1));
                 }
